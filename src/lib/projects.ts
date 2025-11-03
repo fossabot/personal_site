9bfb76897ff803393c51f38bec3449424cd92c39
Karen Mkrtchyan <70455623+KarenMkrtchyan@users.noreply.github.com>
2025-11-03 12:38:43 -0800

    cookin

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,19 +1,19 @@
 const projects = [
-  {
-    title: "FRC Engineering Lead",
-    description: "One of the best teams in california.",
-    image: "frc.jpg",
-  },
-  {
-    title: "Robot dog",
-    description: "C++ & Inverse Kinematics based magic",
-    image: "dog.jpg",
-  },
-  {
-    title: "SaaS Dashboard",
-    description: "Analytics & billing dashboard for a SaaS product.",
-    image: "/images/project3.jpg",
-  },
-];
+	{
+		title: 'FRC Engineering Lead',
+		description: 'One of the best teams in California',
+		image: 'frc.jpg',
+	},
+	{
+		title: 'Robot dog',
+		description: 'C++ & Inverse Kinematics based magic',
+		image: 'dog.jpg',
+	},
+	{
+		title: 'SaaS Dashboard',
+		description: 'Analytics & billing dashboard for a SaaS product.',
+		image: '/images/project3.jpg',
+	},
+]
 
-export { projects };
+export { projects }
